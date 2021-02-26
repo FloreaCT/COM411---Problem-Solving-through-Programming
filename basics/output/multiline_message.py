9d866151d5e6aef17f407a3a1b157903107c9c7c
repl.it user <replituser@example.com>
2021-02-26 18:46:02 +0000

    Added \t before I am...

--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -12,8 +12,8 @@ async def main():
     await say_after(1, ' ')
     await say_after(1, '...rebooting sensory system')
     await say_after(2, '...rebooting output motors')
-    await say_after(3, '...rebooting hover engine\n')
-    await say_after(3, ' ')
+    await say_after(2, '...rebooting hover engine\n')
+    await say_after(2, ' ')
     
 
     print(f"Reboot succesfuly at {time.strftime('%X')}")
@@ -33,4 +33,4 @@ def delay_print(s):
         sys.stdout.flush()
         time.sleep(0.10)
 
-delay_print("I am.......... BEEP!")
+delay_print("\t I am.......... BEEP!")

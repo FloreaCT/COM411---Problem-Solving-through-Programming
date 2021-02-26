6e52f9da11e224ada4e6862eba398864d52ac919
repl.it user <replituser@example.com>
2021-02-26 11:12:30 +0000

    Found BEEP and rebooted him

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,8 @@
+#Display message to standard output in multiple lines of code
+
+print("System Failure Imminent\n")
+print("System Reboot has been initialed..\n")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n")
+print("All systems are ONLINE")
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 print("Hello Master of Coding!")
+print("Second step: git commit -m \"Message!!!\" ")

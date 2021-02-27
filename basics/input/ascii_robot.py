cccb1c218a66bdad73744b0ce91ea104e2a5dfd2
repl.it user <replituser@example.com>
2021-02-27 16:52:15 +0000

    Finally made Beep's face

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,12 @@
+print("Please enter a character for the eyes")
+
+eyes = input()
+
+print("Beep's expression is now as follows")
+print()
+
+x=' '
+print("##########") 
+print("#  " + eyes + 2*x + eyes + "  #") 
+print("# ------ #") 
+print("##########")
---
+++ basics/output/ascii_art.py
@@ -0,0 +1,6 @@
+# Display a box 
+x=' '
+print("##########") 
+print("#  " + eyes + 2*x + eyes + "  #") 
+print("# ------ #") 
+print("##########")

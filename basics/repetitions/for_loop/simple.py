189a96792447b9d1cbb334c2662d22c8ad36eccc
repl.it user <replituser@example.com>
2021-04-29 13:24:15 +0000

    Added code to demonstrate the use of a simple for loop.

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,12 @@
+#Ask user the number of mountains
+print("How many mountains should i display?")
+mountains = int(input())
+
+
+for x in range(mountains):
+  print("           __")
+  print("          /  \_  ")
+  print("         /^    \ ")
+  print("        /  ^    \_")
+  print("      _/ ^ ^     ^\ ")
+  print("     /  ^     ^    \ ")
---
+++ basics/repetitions/while_loop/factorial.py
@@ -0,0 +1,14 @@
+# Ask  for a number
+print("Please enter a number?")
+number = int(input())
+
+# Assign the
+count = 0
+total = 1
+
+# Calculate the factorial
+while ( count < number ):
+    count = count + 1
+    total = total * count
+
+print("The factorial is", total)

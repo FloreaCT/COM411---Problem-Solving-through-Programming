a5c19fe06905ad013a91907a470783d241bc0555
repl.it user <replituser@example.com>
2021-04-29 15:35:34 +0000

    Added code to demonstrate the use of a user-defined function.

--- basics/functions/ascii_character.py
+++ basics/functions/ascii_character.py
@@ -5,7 +5,7 @@ print("Please enter and ASCII code: ")
 ascii_code = int(input())
 
 if ascii_code in range(32,127):
-  print(chr(ascii_code))
+  print("The character represented by the ASCII code", oct(ascii_code)[2:], "is", chr(ascii_code))
 else:
   print("Error! Please enter a value between 32 and 126.")
 
---
+++ basics/functions/simple_function.py
@@ -0,0 +1,10 @@
+#Starting of function
+
+def listen():
+  #Capture user sound
+  print("What sound did I just hear?")
+  sound = input()
+  print("That was a loud", sound + "!")
+
+#Call to function
+listen()
